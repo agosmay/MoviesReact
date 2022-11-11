5c77caccf49981c7660238fa6b78ad0d1db36d90
agosmay <agostinamgatti@gmail.com>
2022-11-11 17:49:53 -0300

    LoginView

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css'
 import MoviesGrid from './features/movies/components/MoviesGrid'
 import SeriesGrid from './features/tv/components/SeriesGrid'
-
+import Login from './features/auth/components/Login'
 
 const App = () => {
   
 
   return (
     <div className="App">
+		<Login />
 		<MoviesGrid />
 		<SeriesGrid />
 		
