0dd2db9cf5c0cedb32562a1bbc83fefd1ee2baa6
agosmay <agostinamgatti@gmail.com>
2022-11-11 18:58:18 -0300

    styledLogin

--- src/features/auth/components/Login.jsx
+++ src/features/auth/components/Login.jsx
@@ -15,12 +15,12 @@ const Login = () => {
 				<p className="tituloLogin">Sign in with your account</p>
 				<form onSubmit={handleSubmit}>
 					<div className="contenedorInput">
-						<label htmlFor="username">Username</label>
-						<input type="text" id="username" className="inputStyle" name="username" onChange={handleChange} value={datos.username} />
+						<label htmlFor="username" className="labelStyle">Username</label>
+						<input type="text" id="username" className="inputStyle" name="username" onChange={handleChange} value={datos.username} required/>
 					</div>
 					<div className="contenedorInput">
 						<label htmlFor="password">Password</label>
-						<input type="password" id="password" className="inputStyle" name="password"  onChange={handleChange} value={datos.password} />
+						<input type="password" id="password" className="inputStyle" name="password"  onChange={handleChange} value={datos.password} required />
 					</div>
 					<div className="botonera">
 						<button type="submit" className="buttonSignIn">Sign in</button>
